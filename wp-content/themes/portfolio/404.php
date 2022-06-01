a4b07df87c98200abd214fbbf838d7b324503ffa
noemievincent <noemie.vincent1@student.hepl.be>
2022-06-01 08:52:17 +0200

    preparing for online deployment

--- wp-content/themes/portfolio/404.php
+++ wp-content/themes/portfolio/404.php
@@ -10,7 +10,7 @@ get_header(); ?>
             <h2 class="error404__title" id="error404" aria-level="2">404</h2>
             <h3 class="error404__subtitle"><?= __('Page non trouvée', 'prt') ?></h3>
         </div>
-        <p class="error404__content"><?= str_replace(':home', '<a href="' . home_url() . '" class="error404__link">' . __('l‘accueil', 'prt') . '</a>', __('Retourner à :home', 'prt')) ?></p>
+        <p class="error404__content"><?= str_replace(':home', '<a href="' . home_url() . '" class="error404__link">' . __('l’accueil', 'prt') . '</a>', __('Retourner à :home', 'prt')) ?></p>
     </section>
 </main>
 <?php get_footer(); ?>
