68586a94a98a6447c4f4013bea8605ede317d782
noemievincent <noemie.vincent1@student.hepl.be>
2022-09-01 12:09:16 +0200

    added iframe tag in single project page

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -8,17 +8,26 @@
         </section>
         <div class="singleProject__container">
             <p class="singleProject__content">
-				<?= get_the_content(); ?>
+                <?= get_the_content(); ?>
             </p>
-            <a href="<?= get_field('link'); ?>"
-               class="singleProject__link btn" target="_blank"><?= __('Visiter le projet', 'prt') ?></a>
-            <figure class="singleProject__fig">
-				<?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'singleProject__thumb']); ?>
-            </figure>
-        </div>
-        <div class="singleProject__nav">
-			<?php prt_previous_post_link('project'); ?>
-			<?php prt_next__post_link('project'); ?>
+            <?php if (get_field('link')): ?>
+                <a href="<?= get_field('link'); ?>"
+                   class="singleProject__link btn" target="_blank"><?= __('Visiter le projet', 'prt') ?></a>
+            <?php endif; ?>
+            <?php if (has_post_thumbnail()): ?>
+                <figure class="singleProject__fig">
+                    <?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'singleProject__thumb']); ?>
+                </figure>
+            <?php else: ?>
+                <div class="iframe__container">
+                    <p class="iframe__info"><?= __('Ceci est un simple aperçu du projet, cliquez sur le bouton <strong>"Visitez le projet"</strong> pour en découvrir davantage.', 'prt') ?></p>
+                    <iframe src="<?= get_field('link'); ?>" class="singleProject__frame" height="700"></iframe>
+                </div>
+            <?php endif; ?>
+            <div class="singleProject__nav">
+                <?php prt_previous_post_link('project'); ?>
+                <?php prt_next__post_link('project'); ?>
+            </div>
         </div>
     </main>
 <?php endwhile; endif; ?>
