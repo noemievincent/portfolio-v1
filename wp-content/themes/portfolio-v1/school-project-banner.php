7c2fed4e87f75d69f00fb7762113dbc2f17951e1
Noémie <noemievincent21@gmail.com>
2025-04-17 14:13:21 +0200

    🚑️ added school-project-banner.php

---
+++ wp-content/themes/portfolio-v1/school-project-banner.php
@@ -0,0 +1,3 @@
+<div id="school-project-banner">
+    <p><?= __('Ce site a été réalisé dans le cadre d’un projet étudiant. Les informations présentées peuvent être incomplètes ou fictives et ne doivent pas être considérées comme exactes ou à jour.', 'prt'); ?></p>
+</div>
